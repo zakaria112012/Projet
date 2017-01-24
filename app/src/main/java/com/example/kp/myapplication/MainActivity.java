c7c2a9c60f4a7682997ec0407d255df849e1e0b4
zakaria112012 <m.zak-3@hotmail.com>
2017-01-24 13:58:34 +0100

    ajout-compile :database

--- app/src/main/java/com/example/kp/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kp/myapplication/MainActivity.java
@@ -63,6 +63,8 @@ public class MainActivity extends AppCompatActivity  {
                 progressDialog.setMessage("Registring user ... ");
                 progressDialog.show();
 
+                progressDialog.setCancelable( true);
+
                 firebaseAuth.createUserWithEmailAndPassword(email,password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
